fbb3b7d32de357230a0436cbb7e590285b9cdb34
Kagisho Mogapi <mogapikagisho@gmail.com>
2022-01-03 14:26:51 +0200

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,82 @@
+﻿
+// Note:PracAss 2 for 2nd semester
+
+// Purpose: A class to manipulate BankAccount information
+
+using System;
+
+
+namespace PracAss3
+{
+    public class BankAccount
+    {
+        //  stores an accumulative total of the balances of all BankAccount objects
+        public static double overallBalance; 
+        string accountHolder; // name of account holder
+        string accountType; // name of account type
+        double balance; // amount of money in the account
+        double creditLimit; // maximum amount that the balance can be in debt
+
+
+        // Non-default constructor
+        public BankAccount(string accHolder, string accType, double creditLimit)
+        {
+            accountHolder = accHolder;
+            accountType = accType;
+            this.creditLimit = creditLimit;
+            balance = 0;
+        }
+
+        // To subtract the balance and OverallBalance with the parameter if account has sufficient funds
+        public string WithDraw(double withdrawalAmt)
+        {
+            string results = "Successful withdrawal";
+
+            double newBalance = balance - withdrawalAmt;
+
+            if (newBalance < creditLimit)
+            {
+                results = "Insufficent funds";
+            }
+            else
+            {
+                balance -= withdrawalAmt;
+                overallBalance -= withdrawalAmt;
+            }
+
+            return results;
+        }
+
+        // Add balance and OverallBalance with the parameter
+        public void Deposit(double depositAmt)
+        {
+            balance += depositAmt;
+            overallBalance += depositAmt;
+        }
+
+        // Display's the details of the bank account
+        public string Details
+        {
+            get
+            {
+                return $"Account Holder: {accountHolder}\n" +
+                  $"Type: {accountType}\n" +
+                  $"Credit Limit: {String.Format("{0:C}", creditLimit)}\n" +
+                  $"Balance: {String.Format("{0:C}", Balance)}\n";
+            }
+        }
+
+        public double Balance { get { return balance; } }
+        public static double OverallBalance { get { return overallBalance; } }
+        public string AccountHolder { set { accountHolder = value; } }
+        public string AccountType { set { accountType = value; } }
+        public double CreditLimit
+        {
+            set
+            {
+                if (value < Balance)
+                    creditLimit = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace PracAss3
+{
+    class Program
+    {
+        static void Main()
+        {
+            BankAccount acount1 = new BankAccount("Nel", "Current", -5000);
+            BankAccount acount2 = new BankAccount("Nel", "Savings", 0);
+            BankAccount acount3 = new BankAccount("Nel", "Credit", -5000);
+            Console.WriteLine(acount1.Details);
+            Console.WriteLine(acount2.Details);
+            Console.WriteLine(acount3.Details);
+            acount1.Deposit(3000);
+            acount2.Deposit(2000);
+            acount3.Deposit(3000);
+            acount1.AccountHolder = "Nel G";
+            acount1.AccountType = "Current (Tjek)";
+            acount2.CreditLimit = -5000;
+
+            Console.WriteLine("Overall Balance: "
+            + BankAccount.OverallBalance.ToString("C"));
+            Console.WriteLine(acount3.WithDraw(2000));
+
+            Console.WriteLine("Account 3 Balance: "
+            + acount3.Balance.ToString("C"));
+            Console.WriteLine("Overall Balance: "
+            + BankAccount.OverallBalance.ToString("C"));
+        }
+    }
+}
